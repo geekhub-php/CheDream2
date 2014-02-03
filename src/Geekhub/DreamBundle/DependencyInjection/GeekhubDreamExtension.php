0bafcbcbd492c851eb0e68a77c89cd366815ceae
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-02-03 02:01:39 +0200

    fix

--- src/Geekhub/DreamBundle/DependencyInjection/GeekhubDreamExtension.php
+++ src/Geekhub/DreamBundle/DependencyInjection/GeekhubDreamExtension.php
@@ -24,4 +24,5 @@ class GeekhubDreamExtension extends Extension
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-    }}
+    }
+}
