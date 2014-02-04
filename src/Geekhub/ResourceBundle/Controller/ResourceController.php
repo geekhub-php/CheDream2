70686573617f40b9e527aad22f019ef854e8ed4b
Oleksandr Moshta <ahonymous@gmail.com>
2014-02-04 17:33:44 +0200

    Created ResourceBundle

---
+++ src/Geekhub/ResourceBundle/Controller/ResourceController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 04.02.14
+ * Time: 17:28
+ */
+
+namespace Geekhub\ResourceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ResourceController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('GeekhubResourceBundle:Resource:index.html.twig');
+    }
+} 
