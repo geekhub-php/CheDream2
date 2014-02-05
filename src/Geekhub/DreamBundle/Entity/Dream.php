ddb929ef8191fad97b94215d8e2e8fb1152b406a
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-02-05 12:20:42 +0200

    add submitted and rejected prop into Dream entity

--- src/Geekhub/DreamBundle/Entity/Dream.php
+++ src/Geekhub/DreamBundle/Entity/Dream.php
@@ -162,6 +162,20 @@ class Dream
     protected $author;
 
     /**
+     * @var boolean
+     *
+     * @ORM\Column(name="submitted", type="boolean")
+     */
+    protected $submitted;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="rejected", type="boolean")
+     */
+    protected $rejected;
+
+    /**
      * Constructor
      */
     public function __construct()
@@ -691,5 +705,51 @@ class Dream
     public function getAuthor()
     {
         return $this->author;
+    }
+
+    /**
+     * Set submitted
+     *
+     * @param boolean $submitted
+     * @return Dream
+     */
+    public function setSubmitted($submitted)
+    {
+        $this->submitted = $submitted;
+
+        return $this;
+    }
+
+    /**
+     * Get submitted
+     *
+     * @return boolean 
+     */
+    public function getSubmitted()
+    {
+        return $this->submitted;
+    }
+
+    /**
+     * Set rejected
+     *
+     * @param boolean $rejected
+     * @return Dream
+     */
+    public function setRejected($rejected)
+    {
+        $this->rejected = $rejected;
+
+        return $this;
+    }
+
+    /**
+     * Get rejected
+     *
+     * @return boolean 
+     */
+    public function getRejected()
+    {
+        return $this->rejected;
     }
 }
