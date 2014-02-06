83fbe27aee3f8d743a7f12d888aa3bc3662d37b7
Sergey Polischuk <spolischook@gmail.com>
2014-02-06 12:23:11 +0200

    Fixed use statement at User entity

--- src/Geekhub/UserBundle/Entity/User.php
+++ src/Geekhub/UserBundle/Entity/User.php
@@ -2,11 +2,10 @@
 
 namespace Geekhub\UserBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Gedmo\Mapping\Annotation as Gedmo;
-//use Geekhub\UserBundle\Entity\DreamUserInterface as DreamUserInterface;
 
 /**
  * Users
