fbbe6b27aa75bbcdb3d889afb2aa443e02b0edfc
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-02-11 10:13:53 +0200

    recovery point into default

--- bin/reload.sh
+++ bin/reload.sh
@@ -5,7 +5,7 @@ php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources
 
 rm -rf app/cache/*
 rm -rf app/logs/*
-
+ 
 APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data' | grep -v root | head -1 | cut -d\  -f1`
 
 setfacl -R -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX app/cache app/logs web/upload
