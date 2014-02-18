fe03d58e7d9fdc44384208bfe034f93e5eb60094
chab <chdn6026@mail.ru>
2014-02-18 18:20:41 +0200

    moved upload directory path to configuration

---
+++ src/Geekhub/UserBundle/Model/FacebookUserInfoLanguageType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Geekhub\UserBundle\Model;
+
+use Geekhub\UserBundle\Model\VkontakteRequestUserData;
+use JMS\Serializer\Annotation\Type;
+
+class FacebookUserInfoLanguageType
+{
+    /**
+     * @Type("integer")
+     */
+	protected $id;
+
+    /**
+     * @Type("string")
+     */
+	protected $name;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+}
--- src/Geekhub/UserBundle/Model/FacebookUserInfoResponse.php
+++ src/Geekhub/UserBundle/Model/FacebookUserInfoResponse.php
@@ -4,6 +4,7 @@ namespace Geekhub\UserBundle\Model;
 
 use Geekhub\UserBundle\Model\VkontakteRequestUserData;
 use JMS\Serializer\Annotation\Type;
+use Geekhub\UserBundle\Model\FacebookUserInfoLanguageType;
 
 class FacebookUserInfoResponse
 {
@@ -62,9 +63,274 @@ class FacebookUserInfoResponse
      */
 	protected $response;
 
+    /**
+     * @Type("string")
+     */
+    protected $locale;
+
+    /**
+     * @Type("Geekhub\UserBundle\Model\FacebookUserInfoLanguageType")
+     */
+    protected $languages;
+
+    /**
+     * @Type("boolean")
+     */
+    protected $verified;
+
+    /**
+     * @Type("string")
+     */
+    protected $updated_time;
+
+    /**
+     * @Type("string")
+     */
+    protected $username;
+
 	public function getBirthday()
 	{
 		return $this->birthday;
 	}
-	//"locale":"en_US","languages":[{"id":"112624162082677","name":"Russian"},{"id":"286242804790413","name":"\u0423\u043a\u0440\u0430\u0438\u043d\u0441\u043a\u0438\u0439."},{"id":"106059522759137","name":"English"}],"verified":true,"updated_time":"2013-11-22T15:58:44+0000","username":"dmitry.chabanenko"}
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $link
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $response
+     */
+    public function setResponse($response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param mixed $timezone
+     */
+    public function setTimezone($timezone)
+    {
+        $this->timezone = $timezone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimezone()
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * @param mixed $website
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * @param mixed $languages
+     */
+    public function setLanguages($languages)
+    {
+        $this->languages = $languages;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguages()
+    {
+        return $this->languages;
+    }
+
+    /**
+     * @param mixed $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param mixed $updated_time
+     */
+    public function setUpdatedTime($updated_time)
+    {
+        $this->updated_time = $updated_time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedTime()
+    {
+        return $this->updated_time;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $verified
+     */
+    public function setVerified($verified)
+    {
+        $this->verified = $verified;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVerified()
+    {
+        return $this->verified;
+    }
+	
 }
