fb37a652ba723889d10a51b15745b40104cd25f7
chab <chdn6026@mail.ru>
2014-02-22 12:00:02 +0200

    used sonatamediabundle (need fixes)

--- src/Geekhub/UserBundle/UserProvider/AbstractUserDataService.php
+++ src/Geekhub/UserBundle/UserProvider/AbstractUserDataService.php
@@ -4,12 +4,15 @@ namespace Geekhub\UserBundle\UserProvider;
 
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Application\Sonata\MediaBundle\Entity\Media;
 
 abstract class AbstractUserDataService
 {
     protected $kernelWebDir;
 
-    protected $imgPath = '/uploads/';
+    protected $imgPath = '/upload/';
+
+    protected $mediaManager;
 
 
     public function setKernelWebDir($kernelWebDir)
@@ -22,8 +25,23 @@ abstract class AbstractUserDataService
         $this->imgPath = $imgPath;
     }
 
+    public function setMediaManager($mediaManager)
+    {
+        $this->mediaManager = $mediaManager;
+    }
+
+
     public function copyImgFromRemote($remoteImg, $localFileName)
     {
+        $media = new Media;
+        $media->setBinaryContent($remoteImg);
+        $media->setProviderName('sonata.media.provider.image');
+ 
+        //$mediaManager = $this->get('sonata.media.manager.media'); // don't need. Injected
+        //$this->mediaManager->save($media); // !!! error! Doesnt work 
+ 
+        return $media;
+        /*
         $content = file_get_contents($remoteImg);
         $destination = $this->kernelWebDir.'/../web'.$this->imgPath;
 
@@ -38,6 +56,7 @@ abstract class AbstractUserDataService
         fclose($fp);
 
         return $this->imgPath.$localFileName;
+        */
     }
 
     abstract function setUserData(UserInterface $user, UserResponseInterface $response);
