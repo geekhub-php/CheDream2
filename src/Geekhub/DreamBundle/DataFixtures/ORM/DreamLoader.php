0ad45075becb03625ab968e5fa7caad7b4b758cb
Oleksandr Moshta <ahonymous@gmail.com>
2014-02-25 00:28:34 +0200

    added first dream

---
+++ src/Geekhub/DreamBundle/DataFixtures/ORM/DreamLoader.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 24.02.14
+ * Time: 22:14
+ */
+
+namespace Geekhub\DreamBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+
+class DreamLoader extends DataFixtureLoader implements OrderedFixtureInterface
+{
+
+    /**
+     * Returns an array of file paths to fixtures
+     *
+     * @return array<string>
+     */
+    protected function getFixtures()
+    {
+        return  array(
+            __DIR__ . '/../Alice/DreamData.yml',
+        );
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 3;
+    }
+}
