1b549e255a3cc70242cd310fbb03778989d219fd
chab <chdn6026@mail.ru>
2014-03-08 10:11:16 +0200

    added controller and  template draft

--- src/Geekhub/UserBundle/Controller/DefaultController.php
+++ src/Geekhub/UserBundle/Controller/DefaultController.php
@@ -10,4 +10,11 @@ class DefaultController extends Controller
     {
         return $this->render('GeekhubUserBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function userViewAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('GeekhubUserBundle:User')->findOneById($id);
+        return $this->render('GeekhubUserBundle:User:view.html.twig', array('user' => $user));
+    }
+
 }
