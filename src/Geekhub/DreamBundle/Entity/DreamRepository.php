89b609606a82749f242346f722e2f858d3f721e1
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-03-11 12:34:15 +0200

    added equipment and work contribution

--- src/Geekhub/DreamBundle/Entity/DreamRepository.php
+++ src/Geekhub/DreamBundle/Entity/DreamRepository.php
@@ -51,11 +51,42 @@ class DreamRepository extends EntityRepository
     public function showFinancialContributors($user, $dream)
     {
         return $this->getEntityManager()
-            ->createQuery('SELECT f.title as article, sum(c.quantity) as zz
+            ->createQuery('SELECT f.title as article, sum(c.quantity) as totalSum
                            FROM GeekhubDreamBundle:FinancialContribute c
                            join c.financialArticle f
                            where c.hiddenContributor = 0 and c.user = ?1 and c.dream = ?2
                            group by f.title
+                           order by f.title
+                           ')
+            ->setParameter(1, $user)
+            ->setParameter(2, $dream)
+            ->getResult();
+    }
+
+    public function showEquipmentContributors($user, $dream)
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT f.title as article, sum(c.quantity) as totalSum, f.quantityType as qType
+                           FROM GeekhubDreamBundle:EquipmentContribute c
+                           join c.equipmentArticle f
+                           where c.hiddenContributor = 0 and c.user = ?1 and c.dream = ?2
+                           group by f.title
+                           order by f.title
+                           ')
+            ->setParameter(1, $user)
+            ->setParameter(2, $dream)
+            ->getResult();
+    }
+
+    public function showWorkContributors($user, $dream)
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT f.title as article, sum(c.quantity) as totalSum, sum(c.quantityDays) as totalDays
+                           FROM GeekhubDreamBundle:WorkContribute c
+                           join c.workArticle f
+                           where c.hiddenContributor = 0 and c.user = ?1 and c.dream = ?2
+                           group by f.title
+                           order by f.title
                            ')
             ->setParameter(1, $user)
             ->setParameter(2, $dream)
--- src/Geekhub/DreamBundle/Twig/ContributionExtension.php
+++ src/Geekhub/DreamBundle/Twig/ContributionExtension.php
@@ -9,6 +9,7 @@
 namespace Geekhub\DreamBundle\Twig;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use Geekhub\DreamBundle\Entity\AbstractContributeResource;
 
 class ContributionExtension extends \Twig_Extension
 {
@@ -28,6 +29,8 @@ class ContributionExtension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFunction('finContribute', array($this, 'finContribute'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('equipContribute', array($this, 'equipContribute'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('workContribute', array($this, 'workContribute'), array('is_safe' => array('html'))),
         );
     }
 
@@ -39,10 +42,45 @@ class ContributionExtension extends \Twig_Extension
         $str = '';
         foreach ($finContr as $fin)
         {
-            $str .= '<li>'.$fin['article'].' '.$fin['zz'].'</li>';
+            $str .= '<li>'.$fin['article'].' '.$fin['totalSum'].'</li>';
         }
         return $str;
-
     }
 
-} 
+    public function equipContribute($user, $dream)
+    {
+        $equipContr = $this->doctrine->getManager()->getRepository('GeekhubDreamBundle:Dream')->showEquipmentContributors($user, $dream);
+
+        $str = '';
+        foreach ($equipContr as $equip)
+        {
+            switch ($equip['qType']) {
+                case AbstractContributeResource::KG:
+                    $qType = 'кг.';
+                    break;
+                case AbstractContributeResource::PIECE:
+                    $qType = 'шт.';
+                    break;
+                case AbstractContributeResource::TON:
+                    $qType = 'тон';
+                    break;
+                default:
+                    $qType = '';
+            }
+            $str .= '<li>'.$equip['article'].' '.$equip['totalSum'].' '.$qType.'</li>';
+        }
+        return $str;
+    }
+
+    public function workContribute($user, $dream)
+    {
+        $workContr = $this->doctrine->getManager()->getRepository('GeekhubDreamBundle:Dream')->showWorkContributors($user, $dream);
+
+        $str = '';
+        foreach ($workContr as $work)
+        {
+            $str .= '<li>'.$work['article'].' '.$work['totalSum'].' чол./ '.$work['totalDays'].' дн.</li>';
+        }
+        return $str;
+    }
+}
