ca81cda19dd1fc154ac71c938b27211f7daeb7d1
Oleksandr Moshta <ahonymous@gmail.com>
2014-03-20 01:43:49 +0200

    beutiful url

--- src/Geekhub/ResourceBundle/Controller/FaqController.php
+++ src/Geekhub/ResourceBundle/Controller/FaqController.php
@@ -12,20 +12,17 @@ class FaqController extends Controller
     /**
      * @Template
      */
-    public function indexAction()
+    public function indexAction($slug)
     {
         $faqs = $this->getDoctrine()->getManager()->getRepository('GeekhubResourceBundle:Faq')
             ->findAll();
+        if (is_null($slug)) {
+            $slug = $faqs[0]->getSlug();
+        }
 
-        return array('faqs' => $faqs);
+        return array(
+            'faqs' => $faqs,
+            'slug' => $slug,
+        );
     }
-//
-//    /**
-//     * @ParamConverter("faq", class="GeekhubResourceBundle:Faq")
-//     * @Template
-//     */
-//    public function showAction(Faq $faq)
-//    {
-//        return array('faq' => $faq);
-//    }
 }
