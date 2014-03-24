53a0998f320438a54c2bbc6c054b0fcf71ca42ef
Sergey Polischuk <spolischook@gmail.com>
2014-03-24 09:55:56 +0200

    Test

--- web/app_dev.php
+++ web/app_dev.php
@@ -16,7 +16,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-var_dump('app_dev.php_start');
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 var_dump('app_dev.php_2');
