25fd6cebc4c4beb21a7428fa157faeef2b76301c
Sergey Polischuk <spolischook@gmail.com>
2014-03-24 12:07:08 +0200

    Test

--- web/app_test.php
+++ web/app_test.php
@@ -6,12 +6,11 @@ $bootstrapPath = realpath(__DIR__.'/../app/bootstrap.php.cache');
 $appKernerl = realpath(__DIR__.'/../app/AppKernel.php');
 
 try {
-    $a = 1 / 0;
+    somevu($test);
 } catch (\Exception $e) {
     echo $e->getMessage();
     var_dump('Catch error');
 }
-$a = 1 / 0;
 
 var_dump($bootstrapPath);
 var_dump($appKernerl);
