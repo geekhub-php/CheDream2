dda45002ed1385d501778d21ab927c6a8a89c98a
Oleksandr Moshta <ahonymous@gmail.com>
2014-03-30 23:38:24 +0300

    added dream status email notification

--- src/Geekhub/DreamBundle/EventListener/DreamSubscriber.php
+++ src/Geekhub/DreamBundle/EventListener/DreamSubscriber.php
@@ -6,6 +6,7 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Geekhub\DreamBundle\Entity\Dream,
     Geekhub\DreamBundle\Entity\Status;
+use Hip\MandrillBundle\Message;
 
 class DreamSubscriber implements EventSubscriber
 {
@@ -43,9 +44,89 @@ class DreamSubscriber implements EventSubscriber
     public function postPersist(LifecycleEventArgs $args)
     {
         $object = $args->getObject();
+        $dispatcher = $this->container->get('hip_mandrill.dispatcher');
 
         if ($object instanceof Status) {
-            // TODO: will make sender emails
+            $dream = $object->getDream();
+            $author = $dream->getAuthor();
+            $contributors = $this->container->get('doctrine')
+                ->getRepository('GeekhubDreamBundle:Dream')
+                ->getArrayContributorsByDream($dream)
+            ;
+
+            switch ($object->getTitle()) {
+                case Status::SUBMITTED :
+                    $this->sendEmail(
+                        $dispatcher,
+                        $dream->getTitle(). ' - створено! Збір ресурсів почнеться після модерації мрії.',
+                        $author->getEmail(),
+                        'STATUS'
+                    );
+                    break;
+                case Status::COLLECTING_RESOURCES :
+                    $this->sendEmail(
+                        $dispatcher,
+                        $dream->getTitle(). ' - розпочато збір коштів!',
+                        $author->getEmail(),
+                        'STATUS'
+                    );
+                    break;
+                case Status::REJECTED :
+                    $this->sendEmail(
+                        $dispatcher,
+                        $dream->getTitle(). ' - повернуто на дооформлення.!',
+                        $author->getEmail(),
+                        'STATUS'
+                    );
+                    break;
+                case Status::IMPLEMENTING :
+                    foreach ($contributors as $contributor) {
+                        $this->sendEmail(
+                            $dispatcher,
+                            $dream->getTitle(). ' - розпочата реалізація',
+                            $contributor->getEmail(),
+                            'STATUS'
+                        );
+                    }
+                    break;
+                case Status::COMPLETED :
+                    $this->sendEmail(
+                        $dispatcher,
+                        $dream->getTitle(). ' - завершена реалізація',
+                        $this->container->getParameter('admin.mail'),
+                        'STATUS'
+                    );
+                    break;
+                case Status::SUCCESS :
+                    $this->sendEmail(
+                        $dispatcher,
+                        'Вітаємо! Вашу мрію - '.$dream->getTitle().' - завершено!',
+                        $author->getEmail(),
+                        'STATUS'
+                    );
+                    break;
+                case Status::FAIL :
+                    $this->sendEmail(
+                        $dispatcher,
+                        'Мрію '.$dream->getTitle().' - потрібно завершити! ',
+                        $author->getEmail(),
+                        'STATUS'
+                    );
+                    break;
+            }
         }
+    }
+
+    protected function sendEmail($dispatcher, $body, $to, $subject)
+    {
+        $message = new Message();
+        $message->setFromEmail('test@gmail.com')
+            ->setFromName('Черкаська мрія')
+            ->addTo($to)
+            ->setSubject($subject)
+            ->setHtml($body)
+        ;
+
+        $dispatcher->send($message);
     }
 }
