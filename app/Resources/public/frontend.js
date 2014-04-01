826b9cc8dc59aeb6ae16cc221477b69f125a078e
daniel <daniel@daniel-laptop.(none)>
2014-04-01 19:58:36 +0300

    dream creation is in progress

--- app/Resources/public/frontend.js
+++ app/Resources/public/frontend.js
@@ -1,5 +1,19 @@
 $(function() {
     
-    console.log('Hello');
+    if ($('.custom-radio').length) {
+        executeCustomRadio();
+    }
+    
+    $(document).on('click', '.custom-radio', function() {
+        executeCustomRadio();
+    });
+    
+    function executeCustomRadio($this) {
+        $('.custom-radio').each(function() {
+            var $this = $(this);
+            $this.toggleClass('active', $this.find('input').prop('checked'));
+            $this.toggleClass('disabled', $this.find('input').prop('disabled'));
+        });
+    }
     
 });
