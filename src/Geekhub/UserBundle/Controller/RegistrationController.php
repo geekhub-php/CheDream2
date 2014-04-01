be9c06c8e9e0a7d87d7f4ffd3f764e7acc708af7
chabanenk0 <chdn6026@mail.ru>
2014-04-01 03:17:37 +0300

    used fosuserbundle registration form

---
+++ src/Geekhub/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Geekhub\UserBundle\Controller;
+
+use Geekhub\UserBundle\Form\UserType;
+use Geekhub\UserBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\Controller\RegistrationController as FosUserBundleRegistrationController;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+
+class RegistrationController extends FosUserBundleRegistrationController
+{
+    public function registerAction(Request $request)
+    {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->CreateForm(new UserType(), $user, array(
+                     'user' => $user,
+                     'media-manager' => $this->container->get('sonata.media.manager.media'),
+                     ));
+
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->container->get('router')->generate('geekhub_dream_homepage');
+                    $response = new RedirectResponse($url);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+        }
+
+        return $this->container->get('templating')->renderResponse('GeekhubUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+
+}
--- src/Geekhub/UserBundle/Form/RegistrationUserType.php
+++ src/Geekhub/UserBundle/Form/RegistrationUserType.php
@@ -10,31 +10,24 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Sonata\MediaBundle\Entity\MediaManager;
 use Geekhub\UserBundle\Entity\User;
 
-class UserType extends AbstractType
+class RegistrationUserType extends AbstractType
 {
     public function getName()
     {
-        return 'newUserForm';
+        return 'geekhub_user_registration';
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         //$transformer = new FileMediaTransformer($options['data']['user'], $options['data']['media-manager']);
         $builder
+            ->add('username', 'text', array('label' => 'username '))
+            ->add('plainPassword', 'text', array('label' => 'password '))
             ->add('firstName', 'text', array('label' => 'Ім\'я '))
             ->add('lastName', 'text', array('label' => 'Прізвище '))
             ->add('birthday', 'birthday', array('label' => 'user.birthday', 'required' => false))
-            ->add('about', 'textarea', array('label' => 'user.about_myself', 'required' => false, 
-                   'attr' => array(
-                       'class'=>'tinymce',
-                       'rows' => 12,
-                    )))
             ->add('email', 'email', array('label' => 'user.email'))
             ->add('phone', 'text', array('label' => 'user.phone'))
-            ->add('skype', 'text', array('label' => 'user.skype', 'required' => false))
-            ->add('facebookUrl', 'url', array('label' => 'user.facebook' , 'required' => false))
-            ->add('vkontakteUrl', 'url', array('label' => 'user.vkontakte', 'required' => false ))
-            ->add('odnoklassnikiUrl', 'url', array('label' => 'user.odnoklassniki', 'required' => false ))
             ->add( $builder->create('avatar', 'hidden')//->addModelTransformer($transformer)
              );
 
@@ -46,11 +39,6 @@ class UserType extends AbstractType
             'data_class' => 'Geekhub\UserBundle\Entity\User',
             'cascade_validation' => true,
             ));
-            //->setRequired(array('user', 'media-manager'))
-            //->setAllowedTypes(array(
-            //    'user' => 'Geekhub\UserBundle\Entity\User',
-            //    'media-manager' => 'Sonata\MediaBundle\Entity\MediaManager',
-            //));
     }
 
 }
