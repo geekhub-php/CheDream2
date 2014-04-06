b87b7000630c9006c8ecfd30b3e9a8fda891b335
Oleksandr Moshta <ahonymous@gmail.com>
2014-04-06 16:22:20 +0300

    edit button to admin dream list

--- src/Geekhub/DreamBundle/Admin/DreamAdmin.php
+++ src/Geekhub/DreamBundle/Admin/DreamAdmin.php
@@ -11,13 +11,22 @@ namespace Geekhub\DreamBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
 
 class DreamAdmin extends Admin
 {
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('slug')
+        ;
+    }
+
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('title', null, array())
+            ->add('title')
             ->add('currentStatus')
         ;
     }
@@ -27,6 +36,19 @@ class DreamAdmin extends Admin
         $listMapper
             ->addIdentifier('title')
             ->add('currentStatus')
+            ->add('slug')
         ;
+    }
+
+    public function getTemplate($name)
+    {
+        switch ($name) {
+            case 'edit':
+                return 'GeekhubDreamBundle:Dream:adminDreamList.html.twig';
+                break;
+            default:
+                return parent::getTemplate($name);
+                break;
+        }
     }
 }
