3c05b3ed1a1b5408d94e25addfd02b4b76ecdbca
Sergey Polischuk <spolischook@gmail.com>
2014-04-23 04:48:44 +0300

    Made ugly changes in RegistrationSubscriber

--- src/Geekhub/UserBundle/EventListener/RegistrationSubscriber.php
+++ src/Geekhub/UserBundle/EventListener/RegistrationSubscriber.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing\Router;
 
 class RegistrationSubscriber
 {
@@ -45,32 +46,40 @@ class RegistrationSubscriber
         }
 
         $request = $this->container->get('request');
-        $url = $this->container->get('router')->generate(self::UPDATE_CONTACTS_ROUTE);
+        $url = $this->container->get('router')->generate(self::UPDATE_CONTACTS_ROUTE, [], Router::ABSOLUTE_URL);
 
-        if ((!$user->getPhone() || !$user->getEmail()) && $request->get('_route') !== self::UPDATE_CONTACTS_ROUTE) {
+        if ((!$user->getEmail() || false !== strpos($user->getEmail(), '@example.com')) && $request->get('_route') !== self::UPDATE_CONTACTS_ROUTE) {
             $event->setResponse(new RedirectResponse($url));
+        } elseif ($accessToken = $request->query->get('code')) {
+            $service = $request->query->get('service');
+            $this->container->get('session')->set('_hwi_oauth.connect_confirmation.key', $accessToken);
+            $event->setResponse($this->container->get('hwi_oauth.connect_controller')->connectServiceAction($request, $service));
         } elseif ($email = $request->query->get('mergeUserWithEmail')) {
             $hasUser = $this->container->get('doctrine')->getRepository('GeekhubUserBundle:User')->findOneByEmail($email);
 
-            if (empty($hasUser->getNotNullSocialIds())) {
+            $socialIds = $hasUser->getNotNullSocialIds();
+            if (empty($socialIds)) {
                 throw new \Exception(sprintf('Oops! Something went wrong - user with email "%s" not register by
                 social networks', $hasUser->getEmail()));
             }
 
             $socialNetworks = $hasUser->getNotNullSocialIds();
             $resourceOwner = $this->container->get(sprintf("hwi_oauth.resource_owner.%s", key($socialNetworks)));
+//            var_dump($resourceOwner->getAuthorizationUrl($url)); exit;
 
 //            $redirectUrl = $this->container->get('security.http_utils')->generateUri($request,
 //                self::UPDATE_CONTACTS_ROUTE);
 //            var_dump($redirectUrl);exit;
 
-            $this->container->get('hwi_oauth.connect_controller')->redirectToServiceAction($request,
-                key($socialNetworks));
-
-
-
 //            var_dump($resourceOwner->getAuthorizationUrl($url)); exit;
-//            $event->setResponse(new RedirectResponse($resourceOwner->getAuthorizationUrl($url)));
+
+//            $this->container->get('hwi_oauth.connect_controller')->connectServiceAction($request, key($socialNetworks));
+
+            $url = $this->container->get('router')->generate(self::UPDATE_CONTACTS_ROUTE, ['service' => key($socialNetworks)], Router::ABSOLUTE_URL);
+            $event->setResponse(new RedirectResponse($resourceOwner->getAuthorizationUrl($url)));
+
+
+
 //            echo $resourceOwner->getAuthorizationUrl($url)."<br/>".$url;
 
 //            $this->container->get('account_merger')->mergeAccountsByEmail($email, $user);
