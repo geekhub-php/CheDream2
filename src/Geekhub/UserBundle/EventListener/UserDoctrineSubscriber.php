07d77c3e0d0951c7d2a7f5360717ff646e723380
Oleksandr Moshta <ahonymous@gmail.com>
2014-04-24 11:00:39 +0300

    done send emails

--- src/Geekhub/UserBundle/EventListener/UserDoctrineSubscriber.php
+++ src/Geekhub/UserBundle/EventListener/UserDoctrineSubscriber.php
@@ -12,33 +12,20 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Geekhub\UserBundle\Entity\User;
+use Hip\MandrillBundle\Dispatcher;
+use Hip\MandrillBundle\Message;
+use Symfony\Component\DependencyInjection\Container;
 
 class UserDoctrineSubscriber implements EventSubscriber
 {
-    public function preUpdate(PreUpdateEventArgs $eventArgs)
-    {
-        $entity = $eventArgs->getEntity();
-//        $entityManager = $args->getEntityManager();
+    protected $mandrillDispatcher;
 
-        if ($entity instanceof User) {
-            if ($eventArgs->hasChangedField('email')) {
-                $entity->setEmail($eventArgs->getOldValue('email'));
-                $fake = $entity->isFakeEmail();
-                $entity->setEmail($eventArgs->getNewValue('email'));
-                if ($fake) {
-                    var_dump($fake, $eventArgs->getOldValue('email'), $eventArgs->getNewValue('email'), $entity->getEmail());
-                    exit;
-                }
-            }
-        }
-    }
+    protected $container;
 
-    public function prePersist(LifecycleEventArgs $args)
+    public function __construct(Dispatcher $mandrillDispatcher, Container $container)
     {
-        $object = $args->getObject();
-        if ($object instanceof User && !$object->isFakeEmail()) {
-            var_dump($object->getEmail());exit;
-        }
+        $this->mandrillDispatcher = $mandrillDispatcher;
+        $this->container = $container;
     }
 
     /**
@@ -52,5 +39,54 @@ class UserDoctrineSubscriber implements EventSubscriber
             'preUpdate',
             'prePersist',
         );
+    }
+
+    public function preUpdate(PreUpdateEventArgs $eventArgs)
+    {
+        $entity = $eventArgs->getEntity();
+
+        if ($entity instanceof User) {
+            if ($eventArgs->hasChangedField('email')) {
+                $entity->setEmail($eventArgs->getOldValue('email'));
+                $fake = $entity->isFakeEmail();
+                $entity->setEmail($eventArgs->getNewValue('email'));
+                if ($fake) {
+                    //this is sending registration email to new user, that had fake email and chenge to not fake
+                    $this->sendEmail($entity);
+                }
+            }
+        }
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $object = $args->getObject();
+        if ($object instanceof User && !$object->isFakeEmail()) {
+            //this is sending registration email to new user, that has not fake email
+            $this->sendEmail($object);
+        }
+    }
+
+    /**
+     * @param User $user
+     */
+    protected function sendEmail(User $user)
+    {
+        $message = new Message();
+        $body = $this->container->get('templating')->render(
+            'GeekhubResourceBundle:Email:registration.html.twig',
+            array(
+                'user' => $user
+            )
+        );
+
+        $message->setFromEmail('test@gmail.com')
+            ->setFromName('Черкаська мрія')
+            ->addTo($user->getEmail())
+            ->setSubject('REGISTRATION')
+            ->setHtml($body)
+        ;
+
+        $this->mandrillDispatcher->send($message);
     }
 }
