37a9de3d6231172445b42808ffd9a3401e1c2a56
Sergey Polischuk <spolischook@gmail.com>
2014-04-26 01:37:36 +0300

    Added deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+php composer.phar update
+php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
+
+rm -rf app/cache/*
+rm -rf app/logs/*
+touch app/logs/error.log
+
+APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data' | grep -v root | head -1 | cut -d\  -f1`
+
+mkdir -p web/upload/media
+rm -rf web/upload/media/*
+rm -rf web/upload/dream/*
+rm -rf web/upload/tmp/*
+
+setfacl -R -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX app/cache app/logs web/upload
+setfacl -dR -m u:"$APACHEUSER":rwX -m u:`whoami`:rwX app/cache app/logs web/upload
+
+php app/console cache:clear
+php app/console assets:install --symlink
+php app/console assetic:dump
+php app/console cache:clear
