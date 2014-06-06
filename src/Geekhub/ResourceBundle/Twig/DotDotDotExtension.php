ac1d7db43931d686c1b6a008cf729d981ebb42c8
Sergey Polischuk <spolischook@gmail.com>
2014-06-06 14:26:14 +0300

    Ignored non windows-1251 symbols (oh! I know that it is terrible)

--- src/Geekhub/ResourceBundle/Twig/DotDotDotExtension.php
+++ src/Geekhub/ResourceBundle/Twig/DotDotDotExtension.php
@@ -13,11 +13,11 @@ class DotDotDotExtension extends \Twig_Extension
 
     public function dotdotdotFilter($text, $number = 20)
     {
-        $decodedText=iconv( mb_detect_encoding($text, mb_detect_order(), true), 'windows-1251', $text);
+        $decodedText=iconv( mb_detect_encoding($text, mb_detect_order(), true), 'windows-1251//IGNORE', $text);
 
         if (mb_strlen($decodedText)>$number) {
             $newText=mb_substr($decodedText,0,$number);
-            $newText=iconv("windows-1251", "UTF-8", $newText);
+            $newText=iconv("windows-1251", "utf-8//IGNORE", $newText);
             $newText=$newText."...";
 
             return $newText;
