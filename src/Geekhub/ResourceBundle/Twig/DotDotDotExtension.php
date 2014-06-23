8470e2447936b5dade52ddc30d717b70c00c3e9b
Dmitry Chabanenko <chabanenk0>
2014-06-24 01:55:07 +0300

    added removal of html tags to dotdotdot filter

--- src/Geekhub/ResourceBundle/Twig/DotDotDotExtension.php
+++ src/Geekhub/ResourceBundle/Twig/DotDotDotExtension.php
@@ -13,14 +13,14 @@ class DotDotDotExtension extends \Twig_Extension
 
     public function dotdotdotFilter($text, $number = 20)
     {
-        $decodedText = $text;
+        $decodedText = trim(strip_tags($text));
         if (mb_strlen($decodedText, 'UTF-8')>$number) {
             $newText=mb_substr($decodedText,0,$number, 'UTF-8');
             $newText=$newText."...";
 
             return $newText;
         } else {
-            return $text;
+            return trim(strip_tags($text));
         }
     }
 
