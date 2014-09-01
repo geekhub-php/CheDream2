816818b4cc5166f9cea404320581a73be74c4a1a
Sergey Polischuk <spolischook@gmail.com>
2014-09-01 15:59:35 +0300

    Fixed mailchimp errors

--- src/Application/MandrillDispatcherForTestEnv.php
+++ src/Application/MandrillDispatcherForTestEnv.php
@@ -7,7 +7,7 @@ use Hip\MandrillBundle\Message;
 
 class MandrillDispatcherForTestEnv extends Dispatcher
 {
-    public function send(Message $message, $templateName = '', $templateContent = array(), $async = false)
+    public function send(Message $message, $templateName = '', $templateContent = array(), $async = false, $ipPool=null, $sendAt=null)
     {
         //ToDo: add write log message functionality
     }
