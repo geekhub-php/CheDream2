755439680f4f3c13335ac28a7ce466041e390ccf
Yaroslav Osypenko <harentius@yandex.ru>
2014-09-03 21:48:38 +0300

    removed unused use

--- src/Geekhub/DreamBundle/Controller/AjaxDreamController.php
+++ src/Geekhub/DreamBundle/Controller/AjaxDreamController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class AjaxDreamController extends Controller
 {
